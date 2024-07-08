749a0752de22385f9c83461e5eb8e6b70711f95f
Asutosh panda <asutoshp97@gmail.com>
2024-07-08 22:53:51 +0530

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id:1,
+    name:"Paris",
+    job:"Monument",
+    image:"https://travel.usnews.com/dims4/USNEWS/8887e35/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2FNew_Paris_pic_jw04ZlH.jpg",
+    text:"France's magnetic City of Light is a perennial tourist destination, drawing visitors with its iconic attractions, like the Eiffel Tower and the Louvre, and its unmistakable je ne sais quoi. Quaint cafes, trendy shopping districts and Haussmann architecture give Paris its timeless beauty. ",
+  },
+  {
+    id:2,
+    name:"Bora Bora",
+    job:"Beach",
+    image:"https://travel.usnews.com/dims4/USNEWS/c399889/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2Fbora_bora_main-2015_WzEwEB3.jpg",
+    text:"What this 12-square-mile French Polynesian island may lack in size it makes up for in sheer tropical beauty. Here, you'll find picturesque beaches, lush jungles and luxurious resorts set on surrounding islets. The island's extinct volcano, Mount Otemanu, makes for a great photo-op or challenging hike, and the friendly Bora Bora locals can help you catch a glimpse of the island's best sights during an off-road excursion.  ",
+  },
+  {
+    id:3,
+    name:"Glacier National Park",
+    job:"National Park",
+    image:"https://travel.usnews.com/dims4/USNEWS/8911536/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2Fmain_getty_3DvcjXU.jpg",
+    text:"Glacier National Park is a 1,583-sq.-mi. wilderness area in Montana's Rocky Mountains, with glacier-carved peaks and valleys running to the Canadian border. It's crossed by the mountainous Going-to-the-Sun Road. Among more than 700 miles of hiking trails, it has a route to photogenic Hidden Lake. ",
+  },
+  {
+    id:4,
+    name:"Rome",
+    job:"Monument",
+    image:"https://travel.usnews.com/dims4/USNEWS/864d205/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2FVladislav_ZolotovGetty_Images2_YDl5rGc.jpg",
+    text:"When you visit Italy's capital city, prepare to cross a few must-see landmarks – including the Colosseum, the Trevi Fountain and the Pantheon – off of your bucket list. Travelers can also see some of Italy's greatest treasures, including St. Peter's Basilica and the Sistine Chapel, in Vatican City.",
+  },
+  {
+    id:5,
+    name:"London",
+    job:"LankMark",
+    image:"https://travel.usnews.com/dims4/USNEWS/44886ac/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2Fgettyimages-482151882_hA4PCda.jpg",
+    text:" At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.",
+  },
+];
+
+export default reviews;
