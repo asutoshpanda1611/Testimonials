fcbbeb59205b112a943eecb8d23df8b9691624f8
Asutosh panda <asutoshp97@gmail.com>
2024-07-08 22:56:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
       <div className='text-center'>
         <h1 className='text-4xl  font-bold'>Travel Destinations</h1>
         <div className='bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto'></div>
-        <Testimonials reviews={reviews}/>        
+        <Testimonials reviews={reviews}/>      
+        {/* {fjk}   */}
       </div>
     </div>
   )
